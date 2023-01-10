daf5b4ace63647673186ab35c00a15ac25a2985a
lgrodriguez <lauragarciarod@gmail.com>
2023-01-10 09:03:20 +0100

    add listTasks feature and view

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -10,21 +10,14 @@ class ApplicationController extends Controller
 {
     function homeAction()
     {
-        //veure que es crea l'objecte Tasks a partir del model creat, i que emmagatzema la informació del JSON
+        $this->listTasksAction();
+    }
+
+    function listTasksAction() {
         $model = new Tasks;
         $this->view->_data = $model->listTasks();
+    }
 
-    }
-    function taskAction()
-    {
-        $this->view;
-    }
-    function savedAction($data = array()){
-        $model = new Tasks;
-        $table = $model->saveData($data);
-        var_dump($table);
-        $this->view;
-    }
     function savedAction($data = array()){
         $model = new Tasks;
         $table = $model->saveData($data);
