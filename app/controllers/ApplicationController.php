1302051cd8dc39fa1002eb876e58e87736f10cf1
lgrodriguez <lauragarciarod@gmail.com>
2023-01-17 10:16:04 +0100

    added error alert

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -113,7 +113,7 @@ class ApplicationController extends Controller
             if ($model->validateLogin()) {
                 header("Location: home");
             } else {
-                echo '<div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-gray-800 dark:text-red-400" role="alert"><span class="font-medium">Danger alert!</span> Change a few things up and try submitting again.</div>';
+                $this->view->__set('error',true);
             }
         }
     }
