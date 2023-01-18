dfa04ab0e12a9f7a999d75c818735070861d0984
lgrodriguez <lauragarciarod@gmail.com>
2023-01-18 10:55:48 +0100

    listTasks

--- app/models/TasksModel.class.php
+++ app/models/TasksModel.class.php
@@ -3,12 +3,14 @@
 /**
  * Tasks class for handling tasks data
  */
-class Tasks extends Model
+class Tasks extends Model implements TasksInterface
 {
     public function __construct()
     {
-        // parse the settings file
-        $this->_dbh = ROOT_PATH . '/web/' . 'db_tasks.json';
+        $settings = parse_ini_file(ROOT_PATH . '/config/settings.ini', true);
+
+        $this->_dbh = mysqli_connect($settings['database']['host'], $settings['database']['user'], $settings['database']['password'], $settings['database']['dbname']);
+        $this->_setTable("tasks");
     }
 
     public function getDB()
@@ -22,7 +24,6 @@ class Tasks extends Model
      */
     public function getData()
     {
-        return (array) json_decode(file_get_contents($this->_dbh, true));
     }
 
     /**
@@ -70,14 +71,12 @@ class Tasks extends Model
      */
     public function listTasks()
     {
-        $data = $this->getData();
+        $query = ("SELECT * FROM " . $this->_table . " WHERE userId = " . $_SESSION['userId']);
+        $data = mysqli_query($this->getDB(), $query);
         $userData = array();
-        foreach ($data as $task) {
-            if (($task->userId == $this->getUserId()) && (isset($task->taskId))) {
-                array_push($userData, $task);
-            }
+        while ($task = mysqli_fetch_object($data)) {
+            array_push($userData, $task);
         }
-
         return $userData;
     }
 
@@ -100,8 +99,9 @@ class Tasks extends Model
         return $filteredUserData;
     }
 
-    public function search($userData, $value){
-        
+    public function search($userData, $value)
+    {
+
         if (!isset($_GET)) {
             $value = null;
         } else {
@@ -121,7 +121,8 @@ class Tasks extends Model
         }
         return $newUserData;
     }
-    public function showSearch($searchedData){
+    public function showSearch($searchedData)
+    {
         $shownData = array();
         foreach ($searchedData as $task) {
             if (($task->userId == $this->getUserId()) && (isset($task->taskId))) {
